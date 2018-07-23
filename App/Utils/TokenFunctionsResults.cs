61379930cf150a56bc5e3bf49434820b123a43c8
DmytroMyloserdov <dagger.dager@gmail.com>
2018-07-23 14:39:11 +0300

    Added sending value through PaymentController

--- App/Utils/TokenFunctionsResults.cs
+++ App/Utils/TokenFunctionsResults.cs
@@ -10,11 +10,11 @@ namespace App.Utils
 {
     public class TokenFunctionsResults<TResult, TPattern> where TPattern : IControllerPattern
     {
-        public static TResult Invoke(UInt64 id, TPattern req, string funcName)
+        public static TResult Invoke(UInt64 id, TPattern req, string funcName, params object[] funcParametrs)
         {
             Crypto.DecryptTwoStringsAndGetContractFunctions(out string senderAddress, req.Sender, out string password, req.Password, req.PassPhrase, out ContractFunctions contractFunctions);
 
-            return contractFunctions.CallFunctionByName<TResult>(senderAddress, password, funcName, id).Result;
+            return contractFunctions.CallFunctionByName<TResult>(senderAddress, password, funcName, id, funcParametrs).Result;
         }
 
         public static TResult Invoke(TPattern req, string funcName)
