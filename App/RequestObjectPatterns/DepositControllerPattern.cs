acbe45e1b06e74945a5cf6d1792d3d305d03e1aa
DmytroMyloserdov <dagger.dager@gmail.com>
2018-07-30 11:42:30 +0300

    Added controller pattern for deposit

---
+++ App/RequestObjectPatterns/DepositControllerPattern.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace App.RequestObjectPatterns
+{
+     public class DepositControllerPattern : IControllerPattern
+    {
+        public string Sender { get; set; }
+        public string Password { get; set; }
+        public string PassPhrase { get; set; }
+        public UInt64 Value { get; set; }
+        public UInt64 Gas { get; set; } = 2100000;
+    }
+}
